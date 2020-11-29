6a15ef74fb39b39cc98d6103bef9d7a7b3caa1d2
Dang Chi <chid@jobready.com.au>
2020-11-29 11:28:43 +0700

    Remove Ingredient

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -9,6 +9,11 @@ const Ingredients = () => {
   const addIngredientHandler = ingredient => {
     setUserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient }]);
   };
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => 
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  }
 
   return (
     <div className="App">
@@ -16,7 +21,7 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
